f53b8722d4182bcb3eb13354be023196c1d92cd4
luckylinux <luckylinux>
2024-03-11 20:10:58 +0100

    Updated Dockerfile and docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Change to app folder
-cd /opt/app
+cd /opt/weewx
 
 # Launch App
-
+echo "Launching docker-entrypoint.sh"
 
 # Infinite loop to debug
-if [[ -v ENABLE_DEBUG_LOOP ]]
-then
-   if [[ "$ENABLE_DEBUG_LOOP" -eq "1" ]]
-   then
+#if [[ -v ENABLE_DEBUG_LOOP ]]
+#then
+#   if [[ "${ENABLE_DEBUG_LOOP}" -eq "1" ]]
+#   then
        while true
        do
           sleep 5
        done
-   fi
-fi
+#   fi
+#fi
